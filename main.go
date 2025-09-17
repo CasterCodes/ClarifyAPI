7b28557b01f98961f393c3bc780c2ab7eb16d606
CasterCodes <nyagucha.kevin.otwori@gmail.com>
2025-09-17 20:11:15 +0300

    Added hello route

--- main.go
+++ main.go
@@ -14,6 +14,10 @@ func main() {
 		return c.SendString("Hello from Fiber on EC2 🚀")
 	})
 
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("Hello route")
+	})
+
 	log.Fatal(app.Listen(":8080"))
 
 }
